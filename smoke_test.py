fde0ddbcd628e254ebfd3c20c292eacb71c421f9
Rahim Iqbal <rahimiqbal21@gmail.com>
2025-11-01 20:48:04 -0500

    chore: initial scaffold

---
+++ smoke_test.py
@@ -0,0 +1,9 @@
+﻿from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+client = OpenAI()
+r = client.chat.completions.create(
+    model="gpt-5-nano",
+    messages=[{"role":"user","content":"Say OK"}],
+)
+print(r.choices[0].message.content)
